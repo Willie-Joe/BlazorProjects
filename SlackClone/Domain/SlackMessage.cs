196b853dfaffd465eb0c663a1ca93cccdd4ffcef
i-will-lie <willie.joe.is@gmail.com>
2021-08-08 01:54:00 +1200

    SlackClone complete

---
+++ SlackClone/Domain/SlackMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlackClone.Domain
+{
+    public class SlackMessage
+    {
+        public string PostedBy { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string Message { get; set; }
+    }
+}
+
