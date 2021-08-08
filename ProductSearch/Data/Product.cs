07bf784354520b8830e162ad6cdc12f6b407ac5c
i-will-lie <willie.joe.is@gmail.com>
2021-08-08 19:58:54 +1200

    ProductSearch Completed

---
+++ ProductSearch/Data/Product.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ProductSearch.Data 
+{ 
+    public class Product
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
+
